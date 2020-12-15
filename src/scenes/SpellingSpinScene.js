b0d111d27a415d922ca857d976cb1ba6a2a2bddf
Matthew Schwencke <matt.schwencke@gmail.com>
2020-12-15 14:18:12 +0900

    A big chunk of work.
    Copied the Grammar falls game and currently modifiying it.
    I should be committing more often, but you know how it goes...

--- src/scenes/SpellingSpinScene.js
+++ src/scenes/SpellingSpinScene.js
@@ -21,13 +21,17 @@ const Y_MAX = 640
 
 const ANSWER_MOVE_TIME = 0.5
 
-const BOTTOM_Y = 540
+const BOTTOM_Y = 500
 
 const NEXT_LEVEL_TARGET = 10
 
-const THIS_GAME = 'Grammar-Falls'
+const ALPHABET = [
+    'A', 'B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+]
 
-export default class GrammarFallsScene extends Phaser.Scene
+const THIS_GAME = 'Spelling-Spin'
+
+export default class SpellingSpinScene extends Phaser.Scene
 {
 	constructor()
 	{
@@ -49,8 +53,8 @@ export default class GrammarFallsScene extends Phaser.Scene
 
 	preload()
     {
-        console.log("Preload Grammar Falls")
-        this.load.json('sentences', 'assets/Sentences.json')
+        console.log("Preload Spelling Spin")
+        this.load.json('J2Ewords', 'assets/J2Ewords.json')
         this.load.image(SKY_KEY, 'assets/night-sky.png')
         this.load.image(UI_KEY, 'assets/BottomMenu.png')
         this.load.spritesheet(EXP_KEY, 'assets/explosion.png', {frameWidth: 64, frameHeight: 64})
@@ -64,15 +68,15 @@ export default class GrammarFallsScene extends Phaser.Scene
 
     create()
     {
-        console.log("Create Grammar Falls")
-        this.gameData = this.cache.json.get('sentences')
+        console.log("Create Spelling Spin")
+        this.gameData = this.cache.json.get('J2Ewords')
         this.createBackground()
-        this.add.image(240, 590, UI_KEY)
+        //this.add.image(240, 590, UI_KEY)
         this.explosion = this.createExplosion()
         this.quiz = {
             sentence: this.createQuizSentence(),
-            answers: this.createAnswers(),
-            correctIndex: -1, // Don't know yet
+            answer: "",
+            letters: this.createLetters(),
             tick: 0,
             sinOffset: 0,
             freq: 0
@@ -82,24 +86,21 @@ export default class GrammarFallsScene extends Phaser.Scene
         this.lives = 3
         this.livesText = this.createLivesText()
         this.lostLife = false
+        this.keys = this.createKeyboardInput()
         this.newQuiz()
-        this.createKeyboardInput()
-        this.selectedAnswer = -1
-        this.isAnswerSelected = false
-        this.createTouchInput(this.quiz.answers)
-        //this.level = 1;
     }
 
 
     update(){
-        if(this.isAnswerSelected){
-            let finished = this.moveAnswer()
-            if(finished){
-                this.checkAnswer(this.selectedAnswer)
-            }
-        }else{
-            this.moveSentence()
-        }
+        // if(this.isAnswerSelected){
+        //     let finished = this.moveAnswer()
+        //     if(finished){
+        //         this.checkAnswer(this.selectedAnswer)
+        //     }
+        // }else{
+        //     this.moveSentence()
+        // }
+        this.moveSentence()
         if(this.quiz.sentence.y > BOTTOM_Y && !this.lostLife){
             this.loseLife()
             this.endQuestion()
@@ -139,22 +140,23 @@ export default class GrammarFallsScene extends Phaser.Scene
     }
 
     createQuizSentence(){
-        let text = this.add.text(X_CENTRE, 240, 'BOO!', {font: FONT_MED}).setOrigin(0.5)
+        let text = this.add.text(X_CENTRE, 240, 'BOO!', {font: FONT_BIG}).setOrigin(0.5)
         this.physics.world.enable(text, 0)
         return text
     }
-    createAnswers() {
-        const numAnswers = 4
-        let answers = []
-        for(let i=0;i<numAnswers;i++){
-            let text = this.add.text(X_CENTRE, 400 + 40 * i, `Ans ${i}`, {font: FONT_MED}).setOrigin(0.5)
+    createLetters() {
+        const numLetters = 10
+        let letters = []
+        for(let i=0;i<numLetters;i++){
+            let text = this.add.text(X_CENTRE, 400 + 40 * i, `${i}`, {font: FONT_BIG}).setOrigin(0.5)
             this.physics.world.enable(text, 0)
             // @ts-ignore
             text.body.setAllowGravity(false)
-            answers.push(text)
+            text.setVisible(false)
+            letters.push(text)
         }
 
-        return answers
+        return letters
     }
 
     createKeyboardInput(){
@@ -175,42 +177,26 @@ export default class GrammarFallsScene extends Phaser.Scene
                 this.pause()
             }
         )
-        keys.one = this.input.keyboard.addKey('ONE')
-        keys.one.on(
-            'down',
-            () => {
-                console.log("ONE pressed")
-                this.selectAnswer(0)
-            }
-        )
-        keys.two = this.input.keyboard.addKey('TWO')
-        keys.two.on(
-            'down',
-            () => {
-                console.log("TWO pressed")
-                this.selectAnswer(1)
-            }
-        )
-        keys.three = this.input.keyboard.addKey('THREE')
-        keys.three.on(
-            'down',
-            () => {
-                console.log("THREE pressed")
-                this.selectAnswer(2)
-            }
-        )
-        keys.four = this.input.keyboard.addKey('FOUR')
-        keys.four.on(
-            'down',
-            () => {
-                console.log("FOUR pressed")
-                this.selectAnswer(3)
-            }
-        )
+        for(let letter in ALPHABET){
+            this.keyboardAddLetter(letter)
+        }
+        
         return keys
     }
 
+    keyboardAddLetter(letter){
+        this.input.keyboard.addKey(letter).on('down',() => {this.checkLetter(letter)})
+    }
 
+    checkLetter(letter){
+        if(this.quiz.answer.includes(letter)){
+            
+        }
+    }
+
+    selectLetter(){
+
+    }
 
     pause() {
         this.scene.pause(THIS_GAME)
@@ -255,22 +241,34 @@ export default class GrammarFallsScene extends Phaser.Scene
     newQuiz(){
         let q = this.getQuestion()
         this.quiz.sentence.text = this.getSentence(q)
-        let ans = this.getAnswers(q)
+        this.quiz.answer = q.english
+        let ls = this.getLetters(q)
+
+        let arc = 2 * Math.PI / ls.length
+        let width = 400
+        let segment = width / ls.length
         
-        this.quiz.correctIndex = ans.correctIndex
-        
-        for(let i=0; i<4; i++){
-            this.quiz.answers[i].setVisible(true)
-            this.quiz.answers[i].text = ans.answers[i]
+        for(let i=0; i<ls.length; i++){
+            this.quiz.letters[i].setVisible(true)
+            this.quiz.letters[i].text = ls[i]
             // @ts-ignore
-            this.quiz.answers[i].body.setVelocity(0)
+            this.quiz.letters[i].body.setVelocity(0)
             // @ts-ignore
-            this.quiz.answers[i].body.setAllowGravity(false)
+            this.quiz.letters[i].body.setAllowGravity(false)
             
-            this.quiz.answers[i].setPosition(
-            /* x */ 120 + 240 * (i % 2), 
-            /* y */ 565 + 50 * (Math.floor(i/2) % 2)                
-            )
+            let centreX = 240
+            let centreY = 565
+            
+            // let radius = 50
+
+            // let x = centreX + radius * Math.sin(arc * i)
+            // let y = centreY + radius * Math.cos(arc * i)
+
+            let x = centreX + segment * (i + .5) - width / 2
+            let y = centreY
+
+            this.quiz.letters[i].setPosition(x, y)
+
         }
         this.quiz.sentence.setVisible(true)
 
@@ -343,7 +341,7 @@ export default class GrammarFallsScene extends Phaser.Scene
 
         if(this.checkForNextLevel()){
             this.time.delayedCall(500, () => {
-                    this.scene.start('Next-Level-Screen', { gameKey: THIS_GAME, score: this.score, level: this.level })
+                    this.scene.start('Next-Level-Screen', { gameKey: 'Grammar-Falls', score: this.score, level: this.level })
                 }, null, this)
         }else{
             this.next()
@@ -386,7 +384,7 @@ export default class GrammarFallsScene extends Phaser.Scene
         if(this.lives < 0){
             //GAME OVER
             this.time.delayedCall(500, () => {
-                this.scene.start('Game-Over-Screen', { gameKey: THIS_GAME, level: this.level, score: this.score })
+                this.scene.start('Game-Over-Screen', { gameKey: 'Grammar-Falls', level: this.level, score: this.score })
             }, null, this)
         }
     }
@@ -400,10 +398,13 @@ export default class GrammarFallsScene extends Phaser.Scene
     }
 
     endQuestion(){
-        if(this.selectedAnswer >= 0){
-            // @ts-ignore
-            this.quiz.answers[this.selectedAnswer].body.setVelocity(0)
-            this.quiz.answers[this.selectedAnswer].setVisible(false)
+        // if(this.selectedAnswer >= 0){
+        //     // @ts-ignore
+        //     this.quiz.answers[this.selectedAnswer].body.setVelocity(0)
+        //     this.quiz.answers[this.selectedAnswer].setVisible(false)
+        // }
+        for(let i=0; i<this.quiz.letters.length; i++){
+            this.quiz.letters[i].setVisible(false)
         }
         this.quiz.sentence.setVisible(false)
         this.isAnswerSelected = false
@@ -413,38 +414,28 @@ export default class GrammarFallsScene extends Phaser.Scene
     //#region Quiz Generators
 
     getQuestion(){
-        let index = this.randIndex(this.gameData.sentences.length);
-        return this.gameData.sentences[index]
+        let index = this.randIndex(this.gameData.quiz.length);
+        return this.gameData.quiz[index]
     }
 
-    /**
-     * @param {{ text: string; }} q
-     */
+
     getSentence(q){
         console.log("GetSentence")
-        let text = this.processText(q.text)
-    
-        return text[0].toUpperCase() + text.slice(1)
-
+        return q.japanese
     }
 
-    getAnswers(q){
-        let correctIndex = -1
-        let indices = ['c', 0, 1, 2]
+    getLetters(q){
+        let indices = []
+        for(let i=0; i<q.english.length; i++){
+            indices.push(i)
+        }
         indices = this.shuffle(indices)
-        let answers = []
-        for(let i=0; i<4;i++){
-            let ans
-            if(indices[i] === 'c'){
-                ans = this.processText(q.correctAnswer)
-                correctIndex = i
-            }else{
-                ans = this.processText(q.wrongAnswer[indices[i]])
-            }
-            answers.push(ans)
+        let letters = []
+        for(let i=0; i<indices.length; i++){
+            letters.push(q.english[indices[i]])
         }
 
-        return { answers, correctIndex }
+        return letters
     }
 
     getLivesString(livesLeft){
@@ -455,39 +446,6 @@ export default class GrammarFallsScene extends Phaser.Scene
         return str;
     }
     
-    /**
-     * @param {string} text
-     */
-    processText(text){
-        if(!text.includes("%")){
-            return text;
-        }
-    
-        // Text includes a % sign
-        //debugger;
-        let finishedText = text;
-        console.log("New Sentence is: " + text);
-        while(finishedText.includes("%")){
-            console.log("Sentence is currently: " + finishedText);
-            let str = finishedText;
-            let wildCard = str.split("%")[1];
-            let replacement = "";
-            
-            for(const partOfSpeech of this.gameData.partsOfSpeech){
-                console.log(partOfSpeech.name);
-                if(wildCard == partOfSpeech.keyword){
-                    console.log("Found keyword "+partOfSpeech.keyword);
-                    replacement = partOfSpeech.words[this.randIndex(partOfSpeech.words.length)];
-                    break;
-                }
-            }
-            
-            finishedText = finishedText.replace("%"+wildCard+"%", replacement);
-        }
-        console.log("Finished sentence is: " + finishedText);
-        return finishedText;
-        
-    }
     randIndex(max){
         return Math.floor(Math.random() * max);
     }
