08a29a1f5906bb6c5ff025b5f8c290e3de43a999
Matthew Schwencke <matt.schwencke@gmail.com>
2020-12-29 17:10:12 +0900

    Added entire python script
    God damn I should have broken up these commits.

---
+++ py/generateJson.py
@@ -0,0 +1,225 @@
+from __future__ import print_function
+import pickle
+import os.path
+import pandas as pd # pylint: disable=import-error
+from googleapiclient.discovery import build # pylint: disable=import-error
+from google_auth_oauthlib.flow import InstalledAppFlow # pylint: disable=import-error
+from google.auth.transport.requests import Request # pylint: disable=import-error
+
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1okniTOvNF_MHrPtzbVgFWVI5TQquTBjNitTRw2kRwZY'
+SAMPLE_RANGE_NAME = 'A2:AA1000'
+
+class Category:
+    def __init__(self, name, key):
+        self.name = name
+        self.key = key
+        self.members = []
+    
+class Quiz:
+    def __init__(self, question, correct, wrongs):
+        self.question = question
+        self.correct = correct
+        self.wrongs = wrongs
+    
+def buildDict(cats):
+    lookup = {}
+    for i in range(0, len(cats)):
+        lookup[cats[i].name] = i
+    
+    return lookup
+
+def OrganiseData(data):
+    print("-----------------------------")
+    print("-----------START-------------")
+    print("-----------------------------")
+
+    quizzes = []
+    categories = []
+    # First, generate categories
+    for row in data:
+        
+        # Are we finished with the categories?
+        if len(row) < 5:
+            break
+        
+        cat = Category(row[5], row[6])
+
+        # category = row[5]
+        # keyword = row[6]
+        subCatStr = ""
+
+        # Are there no subcategories?
+        if len(row) >= 8:
+            subCatStr = row[7]
+            
+            if subCatStr != "":
+                #remove spaces
+                subCatStr = subCatStr.replace(" ", "")
+                #split by comma
+                subCategories = subCatStr.split(",")
+
+                for subCat in subCategories:
+                    key = "%" + subCat + "%"
+                    cat.members.append(key)
+
+        # print('%s, %s, %s' % (cat.name, cat.key, subCatStr))
+
+        categories.append(cat)
+    
+    lookup = buildDict(categories)
+    # print("-----------------------------")
+    #next, find the vocab
+    for row in data:
+        # index = findIndex(categories, row[3])
+        name = row[3]
+        english = row[1]
+
+        index = lookup[name]
+        categories[index].members.append(english)
+        # print('%s, %s, %s' % (row[0], row[1], row[3]))
+    
+    # last, organise the quizzes
+    for row in data:
+        # Are we finished with the quizzes?
+        if len(row) < 9:
+            break
+
+        q = Quiz(row[9], row[10], row[11:14])
+        quizzes.append(q)
+
+    for cat in categories:
+        print(cat.name)
+
+        for member in cat.members:
+            print("    "+member)
+
+    print("-----------------------------")
+    print("------------END--------------")
+    print("-----------------------------")
+
+    organisedData = {
+        "categories": categories, 
+        "quizzes": quizzes
+    }
+
+    return organisedData
+
+def writeJson(data):
+    tab = "    "
+    f = open("test.json", "w", encoding = 'utf-8')
+    lines = []
+    lines.append('{\n')
+
+    #Sentences
+    lines.append(tab+'"sentences":[\n')
+
+    quizzes = []
+
+    for quiz in data["quizzes"]:
+        string = ""
+
+        string += tab*2 + '{\n'
+        string += tab*3 + '"text":             "%s",\n' % (quiz.question)
+        string += tab*3 + '"correctAnswer":    "%s",\n' % (quiz.correct)
+        string += tab*3 + '"wrongAnswer":[     "%s",\n' % (quiz.wrongs[0])
+        
+        string += tab*8+'"%s",\n' % (quiz.wrongs[1])
+        string += tab*8+'"%s"\n' % (quiz.wrongs[2])
+
+        string += tab*3 + ']\n'
+        string += tab*2 + '}'
+
+    
+
+        quizzes.append(string)
+
+    lines.append(',\n'.join(quizzes))
+    lines.append('\n')
+    lines.append(tab+"],\n")
+    lines.append('\n')
+
+    # Parts of Speech
+    lines.append(tab+'"partsOfSpeech":[\n')
+    
+    cats = []
+
+    for cat in data["categories"]:
+        string = ""
+        string += tab*2 + '{\n'
+        string += tab*3 + '"name": "%s",\n' % (cat.name)
+        string += tab*3 + '"keyword": "%s",\n' % (cat.key)
+        string += tab*3 + '"words":[\n'
+        
+        string += tab*4+'"'
+        sep = '",\n'+tab*4+'"'
+        string += sep.join(cat.members)
+        string += '"\n'
+
+        string += tab*3 + ']\n'
+        string += tab*2 + '}'
+        
+        cats.append(string)
+
+    lines.append(',\n'.join(cats))
+    lines.append('\n')
+    lines.append(tab+"]\n")
+
+
+    lines.append("}")
+
+
+    f.writelines(lines)
+    # All finished
+    f.close()
+
+def run(values_input):
+    data = OrganiseData(values_input)
+    writeJson(data)
+
+def main():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    global values_input, service
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result_input = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range=SAMPLE_RANGE_NAME).execute()
+    values_input = result_input.get('values', [])
+
+    if not values_input:
+        print('No data found.')
+    else:
+        run(values_input)
+
+if __name__ == '__main__':
+    main()
+
+df = pd.DataFrame(values_input[1:], columns=values_input[0])
+
