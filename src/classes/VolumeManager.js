606828551f7df1e07adbd14a6ac46c343c638e97
Matthew Schwencke <matt.schwencke@gmail.com>
2021-02-18 15:44:27 +0900

    Seems to work?
    Can't test the actual volume yet, but it drags properly, it seems to work. Will need to test later.

--- src/classes/VolumeManager.js
+++ src/classes/VolumeManager.js
@@ -19,9 +19,10 @@ class VolumeManager {
         console.log("Adding slider!")
         let xMax = 480
         let x = xMax - 20
-        let y = 60
+        let y = 65
+        let h = 100
 
-        let line = context.add.rectangle(x, y, 6, 100)
+        let line = context.add.rectangle(x, y, 6, h)
         line.isFilled = true
         line.fillColor = 0xdddddd
         line.setOrigin(0.5)
@@ -30,6 +31,28 @@ class VolumeManager {
         circle.isFilled = true
         circle.fillColor =0xeeeeee
         circle.setOrigin(0.5)
+        circle.setInteractive()
+
+        
+        let minY = y - h/2
+        let maxY = y + h/2
+        
+        // @ts-ignore
+        context.input.setDraggable(circle)
+        circle.on('drag', (pointer, dragX, dragY) => {
+            let newY = dragY
+            if(dragY > maxY){
+                newY = maxY
+            }else if(dragY < minY){
+                newY = minY
+            }
+
+            circle.y = newY
+            let vol = (maxY - circle.y) / 100
+            this.setVolume(vol)
+            console.log(this.volume)
+        })
+
 
         return {line, circle}
     }
